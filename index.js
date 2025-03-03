904844c40c2a569f249c862837f6ee89a43e3000
Hubert <hubert_piaskowski@o2.pl>
2025-03-03 18:42:35 +0100

    updating login, registration etc

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
+import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "blog",
+  password: "123456",
+  port: 5432,
+});
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,27 +29,77 @@ app.use(
 
 app.set("view engine", "ejs");
 
+// Strona logowania
 app.get("/", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-  const { name } = req.body;
+// Obsługa logowania użytkownika
+app.post("/login", async (req, res) => {
+  const { login, password } = req.body;
 
-  if (!name || name.trim() === "") {
-    return res.send("Proszę podać imię!");
+  if (!login || !password) {
+    return res.send("Proszę podać login i hasło!");
   }
 
-  req.session.name = name;
+  const result = await db.query("SELECT * FROM users WHERE login = $1", [
+    login,
+  ]);
+
+  if (result.rows.length === 0) {
+    return res.send("Nie znaleziono użytkownika!");
+  }
+
+  const user = result.rows[0];
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.send("Nieprawidłowe hasło");
+  }
+
+  req.session.userId = user.id;
+  req.session.userName = user.name;
+  req.session.color = user.color;
+
   res.redirect("/index");
 });
 
+// Strona rejestracji
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+// Obsługa rejestracji użytkownika
+app.post("/register", async (req, res) => {
+  const { name, surname, login, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await db.query(
+      "INSERT INTO users (name, surname, login, password) VALUES ($1, $2, $3, $4)",
+      [name, surname, login, hashedPassword]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.send("Błąd rejestracji - login jest już zajęty");
+  }
+});
+
+// Strona główna
 app.get("/index", (req, res) => {
-  if (!req.session.name) {
+  if (!req.session.userId) {
     return res.redirect("/");
   }
 
-  res.render("index.ejs", { name: req.session.name });
+  res.render("index.ejs", { name: req.session.userName });
+});
+
+// Wylogowanie
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 app.listen(port, () => {
